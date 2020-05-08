766e6d239cb98ff3dc85e6ecc30d69a5931b2ee2
Hare Ram Rai <guru.java.ram@gmail.com>
2020-05-08 22:03:14 +0530

    Abstract update item logic & create string constant

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,52 +1,56 @@
+# frozen_string_literal: true
+
 class GildedRose
+  AGE_BRIE = 'Aged Brie'
+  BACKSTAGE_PASSESS = 'Backstage passes to a TAFKAL80ETC concert'
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'
+
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+      update_item(item)
+    end
+  end
+
+  private
+
+  def update_item(item)
+    if (item.name != AGE_BRIE) && (item.name != BACKSTAGE_PASSESS)
+      if item.quality > 0
+        item.quality = item.quality - 1 if item.name != SULFURAS
+      end
+    else
+      if item.quality < 50
+        item.quality = item.quality + 1
+        if item.name == BACKSTAGE_PASSESS
+          if item.sell_in < 11
+            item.quality = item.quality + 1 if item.quality < 50
           end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+          if item.sell_in < 6
+            item.quality = item.quality + 1 if item.quality < 50
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+    end
+
+    item.sell_in = item.sell_in - 1 if item.name != SULFURAS
+
+    if item.sell_in < 0
+      if item.name != AGE_BRIE
+        if item.name != BACKSTAGE_PASSESS
+          if item.quality > 0
+            if item.name != SULFURAS
+              item.quality = item.quality - 1
             end
-          else
-            item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = item.quality - item.quality
         end
+      else
+        item.quality = item.quality + 1 if item.quality < 50
       end
     end
   end
@@ -60,12 +64,12 @@ class Item
     @sell_in = sell_in
     @quality = quality
   end
-  
+
   def to_h
     { name: @name, sell_in: @sell_in, quality: @quality }
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
